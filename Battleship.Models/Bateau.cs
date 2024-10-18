a5bbaa5ad9e08a8ddce6e581c7d801ccdb7762b1
Axelle-Donot <axelle.donot@hotmail.fr>
2024-10-18 09:55:51 +0200

    add grid for player add oppenent

---
+++ Battleship.Models/Bateau.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace BattleShip.Models
+{
+    public class Bateau
+    {
+        [JsonPropertyName("positionsBateaux")]
+        public List<Dictionary<string, List<string>>> PositionsBateaux { get; set; }
+    }
+}
